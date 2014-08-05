9505dc26c5bfe58fc407631ba496d055232bd773
cmcoffman <clayton.coffman@gmail.com>
2014-08-05 10:36:33 -0400

    working on fitting function

--- monday 2014_08_03/fitter.R
+++ monday 2014_08_03/fitter.R
@@ -2,9 +2,10 @@
 repeat.sunday$GFP.OD=repeat.sunday$GFP/repeat.sunday$OD
 
 #input parameters
-b = 5000        #background ~"y int"
-a = 25000       # highest value minus low (minus b)
-n = 1           #cooperativity - how sigmoidal it is
-k = 1000        #half maximum light intensity
+b = 5000                          #background ~"y int"
+a = 25000                         # highest value minus low (minus b)
+n = 1                             #cooperativity - how sigmoidal it is
+k = 1000                          #half maximum light intensity
+g = repeat.sunday$green.intensity #green intensity values
 
-
+fit=b+a*(((g^n)/((g^n)+(k^n))))
