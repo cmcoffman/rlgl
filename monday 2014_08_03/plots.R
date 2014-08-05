6047de09535c97a270d50045ef23075c077f5f38
cmcoffman <clayton.coffman@gmail.com>
2014-08-04 22:51:05 -0400

    got plots and time series working

--- monday 2014_08_03/plots.R
+++ monday 2014_08_03/plots.R
@@ -1,74 +1,88 @@
 #plots!
 
 require(ggplot2)
-cshl.plot.OD <- ggplot(cshl.all, aes(green.intensity, OD.600))
+
+#repeat-sunday-plots----
+cshl.plot.OD <- ggplot(repeat.sunday, aes(green.intensity, OD.600))
 cshl.plot.OD=cshl.plot.OD +
   geom_point() +
   #scale_x_discrete(limits=c("mock","MeJA")) +
   facet_grid(.~plate) +
-  ggtitle(label="OD vs Intensity for each plate") +
+  ggtitle(label="OD vs Intensity ") +
   xlab("Green Intensity") +
   ylab("OD.600")
 cshl.plot.OD
-ggsave("cshl-plot-OD.png")
+ggsave("repeat-sunday-OD.png")
 
-cshl.plot.GFP <- ggplot(cshl.all, aes(green.intensity, GFP))
+cshl.plot.GFP <- ggplot(repeat.sunday, aes(green.intensity, GFP))
 cshl.plot.GFP=cshl.plot.GFP +
   geom_point() +
   #scale_x_discrete(limits=c("mock","MeJA")) +
   facet_grid(.~plate) +
-  ggtitle(label="GFP vs Intensity for each plate") +
+  ggtitle(label="GFP vs Intensity ") +
   xlab("Green Intensity") +
   ylab("GFP") +
   geom_smooth(method = "loess")
 cshl.plot.GFP
-ggsave("cshl-plot-GFP.png")
+ggsave("repeat-sunday-GFP.png")
 
 
-cshl.plot.GFP.row <- ggplot(cshl.all, aes(factor(row), GFP))
+cshl.plot.GFP.row <- ggplot(repeat.sunday, aes(factor(row), GFP))
 cshl.plot.GFP.row=cshl.plot.GFP.row +
   geom_boxplot() +
   facet_grid(.~plate) +
-  ggtitle(label="GFP by row for each plate") +
+  ggtitle(label="GFP by row ") +
   xlab("Row") +
   ylab("GFP")
 cshl.plot.GFP.row
-ggsave("cshl-plot-GFP-row.png")
+ggsave("repeat-sunday-GFP-row.png")
 
-cshl.plot.OD.row <- ggplot(cshl.all, aes(factor(row), OD.600)) +
+cshl.plot.OD.row <- ggplot(repeat.sunday, aes(factor(row), OD.600)) +
   geom_boxplot() +
   facet_grid(.~plate) +
-  ggtitle(label="OD by row for each plate") +
+  ggtitle(label="OD by row ") +
   xlab("Row") +
   ylab("OD.600")
 cshl.plot.OD.row
-ggsave("cshl-plot-OD-row.png")
+ggsave("repeat-sunday-OD-row.png")
 
-cshl.plot.GFP.column <- ggplot(cshl.all, aes(factor(column), GFP)) +
+cshl.plot.GFP.column <- ggplot(repeat.sunday, aes(factor(column), GFP)) +
   geom_boxplot() +
   facet_grid(.~plate) +
-  ggtitle(label="GFP vs Intensity by column for each plate") +
+  ggtitle(label="GFP vs Intensity by column ") +
   xlab("Column") +
   ylab("GFP")
 cshl.plot.GFP.column
-ggsave("cshl-plot-GFP-column.png")
+ggsave("repeat-sunday-GFP-column.png")
 
 
-cshl.plot.OD.column <- ggplot(cshl.all, aes(factor(column), OD.600)) +
+cshl.plot.OD.column <- ggplot(repeat.sunday, aes(factor(column), OD.600)) +
   geom_boxplot() +
   facet_grid(.~plate) +
-  ggtitle(label="OD vs Intensity by column for each plate") +
+  ggtitle(label="OD vs Intensity by column ") +
   xlab("Column") +
   ylab("OD.600")
 cshl.plot.OD.column
-ggsave("cshl-plot-OD-column.png")
+ggsave("repeat-sunday-OD-column.png")
 
-cshl.plot.OD.vs.GFP <- ggplot(cshl.all, aes(GFP, OD.600)) +
+cshl.plot.OD.vs.GFP <- ggplot(repeat.sunday, aes(GFP, OD.600)) +
   geom_point() +
   facet_grid(.~plate) +
-  ggtitle(label="OD vs GFP for each plate for each plate") +
+  ggtitle(label="OD vs GFP  ") +
   xlab("Column") +
   ylab("OD.600")
 cshl.plot.OD.vs.GFP
-ggsave("cshl-plot-ODvsGFP.png")
+ggsave("repeat-sunday-ODvsGFP.png")
 
+#step-up-plots----
+cshl.plot.GFP <- ggplot(step.up, aes(time, GFP))
+cshl.plot.GFP=cshl.plot.GFP +
+  geom_point() +
+  #scale_x_discrete(limits=c("mock","MeJA")) +
+  facet_grid(.~green.intensity) +
+  ggtitle(label="GFP vs Intensity ") +
+  xlab("Time") +
+  ylab("GFP") +
+  geom_smooth(method = "loess")
+cshl.plot.GFP
+ggsave("repeat-sunday-GFP.png")
